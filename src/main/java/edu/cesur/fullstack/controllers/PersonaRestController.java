a91f9f4d186057c0dec059a1f183fad838a1c5a6
ProfeGM <profegnmf@outlook.com>
2023-10-03 13:44:27 +0200

    Agregado contenido a Clase del martes 03

--- src/main/java/edu/cesur/fullstack/controllers/PersonaRestController.java
+++ src/main/java/edu/cesur/fullstack/controllers/PersonaRestController.java
@@ -1,47 +1,100 @@
 package edu.cesur.fullstack.controllers;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import edu.cesur.fullstack.model.Persona;
 
 @RestController
+@RequestMapping("/personas")
 public class PersonaRestController {
 
-	ArrayList<Persona> listaPersonas = new ArrayList<>(Arrays.asList(new Persona(1, "Juan", "Pérez", 25),
-			new Persona(2, "María", "Gómez", 30), new Persona(3, "Pedro", "González", 28)));
-	// ArrayList<Persona> personas = new ArrayList<>();
+	ArrayList<Persona> listaPersonas = new ArrayList<>(
+			Arrays.asList(new Persona(1, "Juan", "Pérez", 25), new Persona(2, "María", "Gómez", 30),
+					new Persona(3, "Pedro", "González", 28), new Persona(4, "Anna", "Martínez", 28)));
+	
+	
+	
 
-	@GetMapping("/personas/{id}")
-	public Persona getPersona(@PathVariable int id) {
-		for(Persona p : listaPersonas) {
-			if(p.getId() == id) {
-				return p;
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getPersona(@PathVariable int id) {
+		
+		if(id < 0) {
+			return ResponseEntity.badRequest().build();
+		}
+		
+		for (Persona p : listaPersonas) {
+			if (p.getId() == id) {
+				return ResponseEntity.ok(p);
 			}
 		}
-		return null;
+		return ResponseEntity.notFound().build();
 	}
 
-	public Persona createPersona(Persona persona) {
-		return persona;
+	@PostMapping
+	public ResponseEntity<?> createPersona(@RequestBody Persona persona) {
+		listaPersonas.add(persona);
+		
+		URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(persona.getId())
+                .toUri();
+		
+		return ResponseEntity.created(location).build();
 
 	}
 
-	public void DeletePersona(int id) {
+	@DeleteMapping("/{id}")
+	public ResponseEntity deletePersona(@PathVariable int id) {
+		
+		if(id < 0) {
+			return ResponseEntity.badRequest().build();
+		}
+		
+		for (Persona persona : this.listaPersonas) {
+			if (persona.getId()==id) {
+				this.listaPersonas.remove(persona);
+				return ResponseEntity.noContent().build();
+			}
+		}
+		
+		return ResponseEntity.notFound().build();
+		
 
 	}
 
-	public Persona updatePersona(Persona persona) {
-		return null;
+	@PutMapping
+	public ResponseEntity<?> updatePersona(@RequestBody Persona persona) {
+		for (Persona p : listaPersonas) {
+			if (p.getId() == persona.getId()) {
+				p.setName(persona.getName());
+				p.setLastName(persona.getLastName());
+				p.setAge(persona.getAge());
+
+				return ResponseEntity.ok(p);
+			}
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 
-	@GetMapping("/personas")
-	public ArrayList<Persona> getPersonas() {
+	@GetMapping
+	public ResponseEntity<?> getPersonas() {
 
-		return listaPersonas;
+		return ResponseEntity.ok(listaPersonas);
 	}
 
 }
