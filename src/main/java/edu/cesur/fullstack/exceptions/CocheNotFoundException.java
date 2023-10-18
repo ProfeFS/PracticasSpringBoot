81400de69889650004c1fd195dea76013a8a73ea
ProfeGM <profegnmf@outlook.com>
2023-10-18 14:32:43 +0200

    Manejo de excepciones persinalizadas

---
+++ src/main/java/edu/cesur/fullstack/exceptions/CocheNotFoundException.java
@@ -0,0 +1,11 @@
+package edu.cesur.fullstack.exceptions;
+
+
+
+
+public class CocheNotFoundException extends RuntimeException {
+
+	public CocheNotFoundException(String message) {
+        super(message);
+    }
+}
